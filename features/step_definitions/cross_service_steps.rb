24be4e300c85d666c99e657dc3fb42a040258fc3
AhsanZX97 <ahsan97@hotmail.co.uk>
2021-08-06 16:44:20 +0100

    added full journey

--- features/step_definitions/cross_service_steps.rb
+++ features/step_definitions/cross_service_steps.rb
@@ -12,6 +12,13 @@ When('I process the application') do
   find('.govuk-button').click
   fill_in('Reference', :with => @ref_number)
   click_button('Look up')
+  fill_in('How much is the court or tribunal fee?', :with => "200")
+  choose('County' , visible: false, allow_label_click: true)
+  fill_in('Day', :with => date.day)
+  fill_in('Month', :with => date.month)
+  fill_in('Year', :with => date.year)
+  click_button('Next')
+  click_button('Complete processing')
+  expect(page).to have_content 'Eligible for help with fees'
 end
-
 
