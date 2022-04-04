66ae8ca216ccf19b268ad81af5a2e8a7877706d2
Petr Zaparka <petr@zaparka.cz>
2022-04-04 14:14:08 +0100

    RST-4383Fixing bug for authentication token on last page (#537)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,8 @@ class SessionsController < ApplicationController
   # This is implemented as a GET method, because we won't have controll over
   # the page with the Start button that page will be managed by GOV.UK, so
   # we can't make it POST because of XSS restriction
+  skip_before_action :verify_authenticity_token, only: :finish
+
   def start
     storage_with_clear.start
     redirect_to(question_path(QuestionFormFactory::IDS.first))
