006381bb1bb6bd4c1eb9be779e3598420af7a86e
kalvinlyu <kalvin.lyu@solirius.com>
2023-04-24 11:51:44 +0100

    RST-5106-public cucumber tests

--- features/support/page_objects/confirmation_page.rb
+++ features/support/page_objects/confirmation_page.rb
@@ -7,21 +7,16 @@ class ConfirmationPage < BasePage
     element :header_refund, 'h1', text: 'Send your reference number to complete the process'
     element :confirmation_header_paper_one, 'h2', text: 'Your application is not yet complete. You now need to take action.'
     element :confirmation_header_paper_two, 'h2', text: 'You must provide the court or tribunal with this reference number to proceed.'
-    element :confirmation_instruction_line_one, 'p', text: 'Enter this reference number on the help with fees section of your court or tribunal application form'
-    element :confirmation_instruction_line_two, 'p', text: 'If your form does not have a help with fees section, write the reference number at the top of the form.'
-    section :list, '.steps-panel-confirmation' do
-      element :one, 'p', text: 'Reference: '
-      element :two, 'p', text: 'I have completed on online application for help with fees [insert application type and case number if you have one].'
-      element :three, 'p', text: 'Yours sincerely,'
-    end
-    element :next_step, 'p', text: 'We will contact you within 21 days to tell you if you need to provide more information or you need to pay towards your court or tribunal fee.'
+    element :confirmation_instruction_line_one, 'p', text: 'You have completed an application online for help paying a court or tribunal fee and you have been provided with the above reference number for your application.'
+    element :confirmation_instruction_line_two, 'p', text: 'You now need to provide this reference number to the office, court or tribunal that will deal with the claim for which you want help with its fee.'
+    element :next_step, 'p', text: 'They will contact you within 21 days to tell you if you need to provide more information or you need to pay towards your court or tribunal fee.'
     element :feedback_text, 'p', text: 'After you finish your application, you are directed to an optional feedback form.'
     element :finish_application_button, 'input[value="Finish application"]'
 
     element :confirmation_header_online_one, 'h2', text: 'Your application is not yet complete. You now need to take action.'
     element :confirmation_header_online_two, 'h2', text: 'Enter this help with fees reference number in the help with fees section of the online form.'
-    element :confirmation_instruction_online, 'p', text: 'Enter your reference above into the help with fees section of the online form. Your online court application form should look something like this:'
-    element :next_step_online, 'p', text: 'We will contact you if you need to provide more information or you need to pay towards your court or tribunal fee.'
+    element :confirmation_instruction_online, 'p', text: 'You have completed an application online for help paying a court or tribunal fee and you have been provided with the above reference number for your application.'
+    element :next_step_online, 'p', text: 'They will contact you within 21 days to tell you if you need to provide more information or you need to pay towards your court or tribunal fee.'
   end
 
   def submit
