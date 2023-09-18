53989ca585e8d956c12a5995c5d84cdad97e2fca
Petr Zaparka <petr@zaparka.cz>
2023-09-18 11:25:22 +0100

    Switching 1st and 2nd page

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -19,8 +19,8 @@ RSpec.describe SessionsController do
       expect(storage).to have_received(:start)
     end
 
-    it 'redirects to the form_name question' do
-      expect(response).to redirect_to(question_path(:form_name))
+    it 'redirects to the fee question' do
+      expect(response).to redirect_to(question_path(:fee))
     end
   end
 
