69fc392d145332ed5f0398e08b131adc959ddefe
Petr Zaparka <petr@zaparka.cz>
2023-10-18 11:19:39 +0100

    Updating to webmock

--- features/support/webmock.rb
+++ features/support/webmock.rb
@@ -8,16 +8,16 @@ require_relative 'capybara_driver_helper'
 selenium_url = URI.parse ENV.fetch('SELENIUM_URL', 'http://localhost:4444/wd/hub')
 app_host_url = URI.parse Capybara.app_host
 
-stub_request(:get, "https://googlechromelabs.github.io/chrome-for-testing/latest-patch-versions-per-build.json").
-  with(
-    headers: {
-      'Accept' => '*/*',
-      'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      'Host' => 'googlechromelabs.github.io',
-      'User-Agent' => 'Ruby'
-    }
-  ).
-  to_return(status: 200, body: {}.to_json, headers: {})
+# stub_request(:get, "https://googlechromelabs.github.io/chrome-for-testing/latest-patch-versions-per-build.json").
+
+#   with(
+#     headers: {
+#       'Accept' => '*/*',
+#       'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+#       'Host' => 'googlechromelabs.github.io',
+#       'User-Agent' => 'Ruby'
+#     }
+#   ).
 
 stub_request(:post, "https://api.os.uk/oauth2/token/v1").
   with(
@@ -33,4 +33,4 @@ stub_request(:post, "https://api.os.uk/oauth2/token/v1").
   to_return(status: 200, body: "", headers: {})
 
 WebMock.disable_net_connect!(allow_localhost: true, allow: [selenium_url.host, app_host_url.host, 'ondemand.saucelabs.com',
-                                                            'chromedriver.storage.googleapis.com', 'https://messages.cucumber.io/api/reports', 'api.os.uk'])
+                                                            'googlechromelabs.github.io', 'chromedriver.storage.googleapis.com', 'https://messages.cucumber.io/api/reports', 'api.os.uk'])
