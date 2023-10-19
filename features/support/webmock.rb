83bb6837188f95e314bf17b5af14096fdf2ff233
harshniavadhannla <harshni.avadhannla@hmcts.net>
2023-10-19 12:07:26 +0100

    fix api error in smoke tests

--- features/support/webmock.rb
+++ features/support/webmock.rb
@@ -8,4 +8,5 @@ require_relative 'capybara_driver_helper'
 selenium_url = URI.parse ENV.fetch('SELENIUM_URL', 'http://localhost:4444/wd/hub')
 app_host_url = URI.parse Capybara.app_host
 
-WebMock.disable_net_connect!(allow_localhost: true, allow: [selenium_url.host, app_host_url.host, 'https://messages.cucumber.io/api/reports'])
+# WebMock.disable_net_connect!(allow_localhost: true, allow: [selenium_url.host, app_host_url.host, 'https://messages.cucumber.io/api/reports'])
+WebMock.allow_net_connect!
