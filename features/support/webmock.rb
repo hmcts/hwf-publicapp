39c97f62700294f6a8a0995035ef11e4d6560fde
harshniavadhannla <harshni.avadhannla@hmcts.net>
2023-10-19 12:39:29 +0100

    fix rubocop error

--- features/support/webmock.rb
+++ features/support/webmock.rb
@@ -5,8 +5,8 @@ include WebMock::API
 
 require_relative 'capybara_driver_helper'
 
-selenium_url = URI.parse ENV.fetch('SELENIUM_URL', 'http://localhost:4444/wd/hub')
-app_host_url = URI.parse Capybara.app_host
+URI.parse ENV.fetch('SELENIUM_URL', 'http://localhost:4444/wd/hub')
+URI.parse Capybara.app_host
 
 # WebMock.disable_net_connect!(allow_localhost: true, allow: [selenium_url.host, app_host_url.host, 'https://messages.cucumber.io/api/reports'])
 WebMock.allow_net_connect!
