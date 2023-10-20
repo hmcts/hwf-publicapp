29d24fa0e6e84240f750a00079405a8702304c6e
Petr Zaparka <petr@zaparka.cz>
2023-10-20 09:42:00 +0100

    Updating title copy

--- features/support/page_objects/fee_page.rb
+++ features/support/page_objects/fee_page.rb
@@ -4,7 +4,7 @@ class FeePage < BasePage
 
   section :content, '#content' do
     element :step_info, '.govuk-caption-l', text: 'Step 1 of 22'
-    element :header, 'h1', text: 'Have you already paid the fee?'
+    element :header, 'h1', text: 'Have you already paid the court or tribunal fee?'
     element :apply_for_refund, '.govuk-body', text: 'You can apply for a refund for a fee paid in the last 3 months'
     element :format_error_link, 'a', text: 'Enter the date in this format DD/MM/YYYY'
     element :format_error_message, '.error-message', text: 'Enter the date in this format DD/MM/YYYY'
