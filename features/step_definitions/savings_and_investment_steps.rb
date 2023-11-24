63bbb68dcd6406a7cbbe9fd0dce65c69f8a6052e
Petr Zaparka <petr@zaparka.cz>
2023-11-24 16:11:56 +0000

    No step info

--- features/step_definitions/savings_and_investment_steps.rb
+++ features/step_definitions/savings_and_investment_steps.rb
@@ -1,7 +1,7 @@
 Given(/^I am a single person on the savings and investments page$/) do
   to_single_savings
   expect(savings_investment_page).to be_displayed
-  expect(savings_investment_page.content).to have_step_info
+  # expect(savings_investment_page.content).to have_step_info
   expect(savings_investment_page.content).to have_header
   expect(savings_investment_page.content).to have_hint_text
 end
@@ -9,7 +9,7 @@ end
 Given(/^I am a single person on the savings and investments page ucd$/) do
   to_single_savings_ucd
   expect(savings_investment_page).to be_displayed
-  expect(savings_investment_page.content).to have_step_info_ucd
+  # expect(savings_investment_page.content).to have_step_info_ucd
   expect(savings_investment_page.content).to have_header_ucd
   expect(savings_investment_page.content).to have_single_reminder
 end
@@ -17,7 +17,7 @@ end
 Given(/^I am a married person on the savings and investments page$/) do
   to_married_savings
   expect(savings_investment_page).to be_displayed
-  expect(savings_investment_page.content).to have_step_info
+  # expect(savings_investment_page.content).to have_step_info
   expect(savings_investment_page.content).to have_header
   expect(savings_investment_page.content).to have_married_reminder
 end
@@ -25,7 +25,7 @@ end
 Given(/^I am a married person on the savings and investments page ucd$/) do
   to_married_savings_ucd
   expect(savings_investment_page).to be_displayed
-  expect(savings_investment_page.content).to have_step_info_ucd
+  # expect(savings_investment_page.content).to have_step_info_ucd
   expect(savings_investment_page.content).to have_header_ucd
   expect(savings_investment_page.content).to have_married_reminder_ucd
 end
