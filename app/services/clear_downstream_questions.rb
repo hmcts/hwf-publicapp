68e0146e6bef29bc3716b8c9b6fa289ffc55080b
Petr Zaparka <petrzaparka@Petrs-MacBook-Air.local>
2023-12-04 16:34:26 +0000

    Refactoring downstream clean method

--- app/services/clear_downstream_questions.rb
+++ app/services/clear_downstream_questions.rb
@@ -6,89 +6,92 @@ class ClearDownstreamQuestions
     @question = question
   end
 
-  # TODO: refactor
-  # rubocop:disable Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/AbcSize
   def for_changes(old_online_application, new_online_application)
-    if dependent_change?(new_online_application, old_online_application) ||
-       income_kind_change?(new_online_application, old_online_application)
-      @storage.clear_forms([:income_range, :income_amount])
-    elsif income_range_change?(new_online_application, old_online_application)
-      @storage.clear_form(:income_amount)
-    elsif benefit_change?(new_online_application)
-      @storage.clear_forms([:income_range, :income_amount, :income_kind, :dependent])
-    elsif legal_representative_changed?(new_online_application, old_online_application)
-      clear_legal_representative_details
-    elsif applying_on_behalf_changed?(new_online_application, old_online_application)
-      clear_applying_on_behalf
-    elsif over_16_changed?(new_online_application, old_online_application)
-      clear_over_16_related_data(new_online_application)
-    elsif married_changed?(new_online_application, old_online_application)
-      clear_partner_data
-    elsif !old_online_application.ni_number_present.nil?
-      clear_ni_or_ho(old_online_application)
+    @old_online_application = old_online_application
+    @new_online_application = new_online_application
+
+    if respond_to?("#{@question}_change?") &&
+       send("#{@question}_change?")
+      send("#{@question}_clear")
     end
   end
-  # rubocop:enable Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/AbcSize
 
-  def benefit_change?(new_online_application)
-    @question == :benefit && new_online_application.benefits == true
-  end
-
-  def income_kind_change?(new_online_application, old_online_application)
-    @question == :income_kind &&
-      old_online_application.income != 0 &&
-      new_online_application.income&.zero?
-  end
-
-  def income_range_change?(new_online_application, old_online_application)
-    (@question == :income_range &&
-      old_online_application.income_min_threshold_exceeded !=
-        new_online_application.income_min_threshold_exceeded) ||
-      old_online_application.income_max_threshold_exceeded !=
-        new_online_application.income_max_threshold_exceeded
-  end
-
-  def dependent_change?(new_online_application, old_online_application)
+  def dependent_change?
     @question == :dependent &&
-      old_online_application.children != new_online_application.children
+      @old_online_application.children != @new_online_application.children
   end
 
-  def clear_ni_or_ho(old_online_application)
-    if old_online_application.ni_number_present == false && old_online_application.ni_number
+  def dependent_clear
+    @storage.clear_forms([:income_range, :income_amount])
+  end
+  alias income_kind_clear dependent_clear
+
+  def income_kind_change?
+    @question == :income_kind &&
+      @old_online_application.income != 0 &&
+      @new_online_application.income&.zero?
+  end
+
+  def income_range_change?
+    (@question == :income_range &&
+      @old_online_application.income_min_threshold_exceeded !=
+        @new_online_application.income_min_threshold_exceeded) ||
+      @old_online_application.income_max_threshold_exceeded !=
+        @new_online_application.income_max_threshold_exceeded
+  end
+
+  def income_range_clear
+    @storage.clear_form(:income_amount)
+  end
+
+  def benefit_change?
+    @question == :benefit && @new_online_application.benefits == true
+  end
+
+  def benefit_clear
+    @storage.clear_forms([:income_range, :income_amount, :income_kind, :dependent])
+  end
+
+  def dob_change?
+    !@old_online_application.ni_number_present.nil?
+  end
+
+  def dob_clear
+    if @old_online_application.ni_number_present == false && @old_online_application.ni_number
       @storage.clear_form(:national_insurance)
-    elsif old_online_application.ni_number && old_online_application.ho_number
+    elsif @old_online_application.ni_number && @old_online_application.ho_number
       @storage.clear_form(:home_office)
     end
   end
 
-  def legal_representative_changed?(new_online_application, old_online_application)
-    return false if @question != :legal_representative || old_online_application.legal_representative.nil?
+  def legal_representative_change?
+    return false if @question != :legal_representative || @old_online_application.legal_representative.nil?
 
-    new_online_application.legal_representative != old_online_application.legal_representative
+    @new_online_application.legal_representative != @old_online_application.legal_representative
   end
 
-  def clear_legal_representative_details
+  def legal_representative_clear
     @storage.clear_form(:legal_representative_detail)
   end
 
-  def applying_on_behalf_changed?(new_online_application, old_online_application)
-    return false if @question != :applying_on_behalf || old_online_application.applying_on_behalf.nil?
+  def applying_on_behalf_change?
+    return false if @question != :applying_on_behalf || @old_online_application.applying_on_behalf.nil?
 
-    new_online_application.applying_on_behalf != old_online_application.applying_on_behalf
+    @new_online_application.applying_on_behalf != @old_online_application.applying_on_behalf
   end
 
-  def clear_applying_on_behalf
+  def applying_on_behalf_clear
     @storage.clear_form(:legal_representative_detail)
     @storage.clear_form(:legal_representative)
   end
 
-  def over_16_changed?(new_online_application, old_online_application)
+  def over_16_change?
     return false if @question != :over_16
 
-    new_online_application.over_16 != old_online_application.over_16
+    @new_online_application.over_16 != @old_online_application.over_16
   end
 
-  def clear_over_16_related_data(_new_online_application)
+  def over_16_clear
     form = Forms::MaritalStatus.new({ married: false })
     @storage.save_form(form)
 
@@ -97,10 +100,14 @@ class ClearDownstreamQuestions
     @storage.clear_form(:home_office)
   end
 
-  def married_changed?(new_online_application, old_online_application)
+  def married_changed?
     return false if @question != :marital_status
 
-    new_online_application.married != old_online_application.married
+    @new_online_application.married != @old_online_application.married
+  end
+
+  def married_clear
+    clear_partner_data
   end
 
 end
