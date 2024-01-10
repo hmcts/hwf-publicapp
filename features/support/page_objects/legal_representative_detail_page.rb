ff63a7d3b0e450a08e9fba2055d76dd0f75858be
Kalvin Lyu <104897244+kalvinSolirius@users.noreply.github.com>
2024-01-10 09:25:50 +0000

    RST-6065 added welsh for representative pages content (#747)
    * RST-6065 added welsh for representative pages content
    
    * RST-6065 added welsh for over_16 inclusion
    
    * RST-6065 added welsh for other error messages
    
    * RST-6065 added missing content and translation
    
    * RST-6065 added missing additional content and translation
    
    * RST-6065 check if smoke test pass
    
    * RST-6065 undo check

--- features/support/page_objects/legal_representative_detail_page.rb
+++ features/support/page_objects/legal_representative_detail_page.rb
@@ -10,8 +10,8 @@ class LegalRepresentativeDetailPage < BasePage
     fill_in 'First name', with: 'Tom'
     fill_in 'Last name', with: 'Jones'
     fill_in 'Email address', with: 'tom@jones.com'
-    fill_in 'Street', with: 'London road'
-    fill_in 'Town', with: 'Bath'
+    fill_in 'House number and street', with: 'London road'
+    fill_in 'Town or City', with: 'Bath'
     fill_in 'Postcode', with: 'ABC132'
 
     continue
--- features/support/page_objects/national_insurance_presence_page.rb
+++ features/support/page_objects/national_insurance_presence_page.rb
@@ -5,10 +5,10 @@ class NationalInsurancePresencePage < BasePage
   section :content, '#content' do
     element :step_info, '.govuk-caption-l', text: 'Step 3 of 22'
     element :header, 'h1', text: 'Do you have a National Insurance number?'
-    element :hint, 'div.govuk-hint', text: 'Select one'
+    element :hint, 'div.govuk-hint', text: 'National Insurance numbers can be found on payslips or official letters about tax, pensions or benefits.'
     element :yes, 'label', text: 'Yes'
     element :no, 'label', text: 'No'
-    element :where_to_find, 'summary', text: 'Where to find my National Insurance number?'
+    element :where_to_find, 'summary', text: 'Find a lost National Insurance number'
     element :look_for_ni_text, 'li', text: 'look for your National Insurance number on payslips or official letters about tax, pensions or benefits'
     element :ask_for_reminder_link, 'a', text: 'ask for a reminder through the post'
     section :alert, 'div[role="alert"]' do
