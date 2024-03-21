2660b72c437aae41b9a78e9d85a1bad9e94de0e4
Aaron Lambley <aaron.lambley@gmail.com>
2024-03-21 16:58:04 +0000

    Add config to prevent html from being wrapped in form_with_error divs

--- config/application.rb
+++ config/application.rb
@@ -69,5 +69,9 @@ module HwfPublicapp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+      html_tag
+    }
   end
 end
--- features/step_definitions/summary_steps.rb
+++ features/step_definitions/summary_steps.rb
@@ -71,7 +71,7 @@ And(/^I visit the start session path$/) do
 end
 
 Then(/^I expect to have a blank form number$/) do
-  form_name_page.should have_field("form_name[identifier]", text: "")
+  find('#form_name_identifier').value.should eq("")
 end
 
 And(/^I change the benefit status$/) do
