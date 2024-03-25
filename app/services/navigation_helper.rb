f77cdb28cdfc4c482bfd5856b474b8b04a1795d0
Aaron Lambley <42571140+lambley@users.noreply.github.com>
2024-03-25 11:27:35 +0000

    Disable Contact page if legal representative selected (#804)

--- app/services/navigation_helper.rb
+++ app/services/navigation_helper.rb
@@ -95,13 +95,12 @@ module NavigationHelper
   end
 
   def legal_representative?
-    return false if @current_question != :applying_on_behalf
+    next_pages = {
+      applying_on_behalf: @online_application.applying_on_behalf ? :legal_representative : :national_insurance_presence,
+      applicant_address: @online_application.legal_representative ? :apply_type : :contact
+    }
 
-    @next_page = if @online_application.applying_on_behalf
-                   :legal_representative
-                 else
-                   :national_insurance
-                 end
+    @next_page = next_pages[@current_question] || false
   end
 
   def over_16?
