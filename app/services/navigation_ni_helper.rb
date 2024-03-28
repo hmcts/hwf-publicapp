ad5b0b9e76120bde12a3169b02137c9fa8722acf
Aaron Lambley <aaron.lambley@gmail.com>
2024-03-28 10:31:49 +0000

    Fix next form step after national insurance

--- app/services/navigation_ni_helper.rb
+++ app/services/navigation_ni_helper.rb
@@ -3,6 +3,10 @@ module NavigationNiHelper
     @online_application.ni_number_present == false ? :home_office : :national_insurance
   end
 
+  def ucd_no_ni_number_page
+    @online_application.ni_number_present == false ? :home_office : :marital_status
+  end
+
   def ucd_ni_next_question
     if ucd_apply?(@online_application.calculation_scheme) && !@online_application.applying_on_behalf
       :national_insurance
@@ -12,14 +16,12 @@ module NavigationNiHelper
   end
 
   def ni_related_question?
-    case @current_question
-    when :applying_on_behalf
-      @ni_next_page = ucd_ni_next_question
-    when :national_insurance_presence
-      @ni_next_page = no_ni_number_page
-    when :national_insurance
-      @ni_next_page = :marital_status
-    end
+    next_pages = {
+      applying_on_behalf: ucd_ni_next_question,
+      national_insurance_presence: no_ni_number_page,
+      national_insurance: ucd_no_ni_number_page
+    }
+    @ni_next_page = next_pages[@current_question]
   end
 
   def partner_ni_related_question?
