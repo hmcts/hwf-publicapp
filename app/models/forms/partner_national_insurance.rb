c924d3921f4b7fc3f98a5e96d99698d437942e13
emilysatodossantos <emily.satodossantos@solirius.com>
2024-06-18 13:06:24 +0100

    Rename variable to reflect expected value

--- app/models/forms/partner_national_insurance.rb
+++ app/models/forms/partner_national_insurance.rb
@@ -3,11 +3,11 @@ module Forms
     include ActiveModel::Validations::Callbacks
 
     attribute :number, String
-    attribute :partner_ni_number_present, Boolean
+    attribute :partner_ni_number_blank, Boolean
 
     NI_NUMBER_REGEXP = /\A(?!BG|GB|NK|KN|TN|NT|ZZ)[ABCEGHJ-PRSTW-Z][ABCEGHJ-NPRSTW-Z]\d{6}[A-D]\z/
 
-    validates :number, presence: true, unless: ->(form) { form.partner_ni_number_present }
+    validates :number, presence: true, unless: ->(form) { form.partner_ni_number_blank }
     validates :number, format: { with: NI_NUMBER_REGEXP }, allow_blank: true
 
     before_validation :format_number
@@ -23,8 +23,8 @@ module Forms
 
     def export_params
       {
-        partner_ni_number: partner_ni_number_present ? nil : number,
-        partner_ni_number_present: partner_ni_number_present
+        partner_ni_number: partner_ni_number_blank ? nil : number,
+        partner_ni_number_blank: partner_ni_number_blank
       }
     end
   end
--- features/support/page_objects/partner_national_insurance_page.rb
+++ features/support/page_objects/partner_national_insurance_page.rb
@@ -6,18 +6,18 @@ class PartnerNationalInsurancePage < BasePage
     element :header, 'h1', text: "Enter your partner's National Insurance number"
     element :national_insurance_text, 'div.govuk-hint', text: 'For example, QQ123456C'
     element :national_insurance_number, 'partner_national_insurance[number]'
-    element :partner_ni_number_present, 'label', text: 'My partner does not have a National Insurance number'
+    element :partner_ni_number_blank, 'label', text: 'My partner does not have a National Insurance number'
   end
 
   def submit_valid_ni
-    partner_national_insurance_page.content.partner_ni_number_present.click
+    partner_national_insurance_page.content.partner_ni_number_blank.click
     # partner_national_insurance_page.content.national_insurance_number
     # find_field('partner_national_insurance[number]', visible: false).set('JL806367D')
     continue
   end
 
   def submit_no_ni
-    content.partner_ni_number_present.click
+    content.partner_ni_number_blank.click
     continue
   end
 
