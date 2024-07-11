59d016d6fcad2a67bb827b59d0140e31171df710
emilysatodossantos <emily.satodossantos@solirius.com>
2024-07-11 12:26:19 +0100

    Add validation for ni number duplicate

--- app/models/forms/partner_national_insurance.rb
+++ app/models/forms/partner_national_insurance.rb
@@ -2,6 +2,7 @@ module Forms
   class PartnerNationalInsurance < Base
     include ActiveModel::Validations::Callbacks
 
+    attribute :ni_number, String
     attribute :number, String
     attribute :partner_ni_number_blank, Boolean
 
@@ -10,9 +11,17 @@ module Forms
     validates :number, presence: true, unless: ->(form) { form.partner_ni_number_blank }
     validates :number, format: { with: NI_NUMBER_REGEXP }, allow_blank: true
 
+    validate :number_duplicate
+
     before_validation :format_number
 
     private
+
+    def number_duplicate
+      if number.present? && number == ni_number
+        errors.add(:number, :not_duplicate)
+      end
+    end
 
     def format_number
       unless number.nil?
