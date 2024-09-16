891c042b4aae8c637b6b945d7f671bf65bd75c69
Aaron Lambley <aaron.lambley@gmail.com>
2024-09-16 16:24:19 +0100

    ucd update - update rspec feature tests

--- spec/features/pages/dependent_question_spec.rb
+++ spec/features/pages/dependent_question_spec.rb
@@ -8,13 +8,11 @@ RSpec.feature 'As a user' do
 
     context 'completing the form correctly' do
       before do
-        choose :dependent_children_true
-        fill_in :dependent_children_number, with: '1'
-        click_button 'Continue'
+        fill_dependent(true)
       end
 
       scenario 'I expect to be routed to the "income_kind" page' do
-        expect(page).to have_content 'What kind of income did you receive last month?'
+        expect(page).to have_content 'Types of income you and your partner have received'
       end
     end
 
@@ -29,7 +27,7 @@ RSpec.feature 'As a user' do
         end
 
         scenario 'I expect the fields to have specific errors' do
-          expect(page).to have_xpath('//span[@class="govuk-error-message"]', text: 'You need to say whether you have financially dependent children')
+          expect(page).to have_xpath('//a[@class="error-link"]', text: 'You need to say whether you have financially dependent children')
         end
       end
 
@@ -40,18 +38,19 @@ RSpec.feature 'As a user' do
           before { click_button 'Continue' }
 
           scenario 'I expect the fields to have specific errors' do
-            expect(page).to have_xpath('//span[@class="govuk-error-message"]', text: 'You must enter the number of financially dependent children')
+            expect(page).to have_xpath('//a[@class="error-link"]', text: 'Enter number of children in this age category')
+            expect(page).to have_xpath('//a[@class="error-link"]', text: 'You must enter a number greater than 0')
           end
         end
 
         context 'entering text as children_number input' do
           before do
-            fill_in :dependent_children_number, with: 'foo'
+            fill_in :dependent_children_age_band_one, with: 'foo'
             click_button 'Continue'
           end
 
           scenario 'I expect the fields to have specific errors' do
-            expect(page).to have_xpath('//span[@class="govuk-error-message"]', text: 'You must enter financially dependent children as a number')
+            expect(page).to have_xpath('//a[@class="error-link"]', text: 'You must enter a number greater than 0')
           end
         end
       end
