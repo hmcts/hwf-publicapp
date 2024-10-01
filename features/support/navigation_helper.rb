eeead25b1886e4dda997a7fcc59186027c923e56
cameron-solirius <cameron.browne@solirius.com>
2024-10-01 11:40:49 +0100

    contact_single.feature tests

--- features/support/navigation_helper.rb
+++ features/support/navigation_helper.rb
@@ -229,7 +229,7 @@ def to_online_confirmation_page
   summary_page.submit_application
 end
 
-def to_contact_page
+def to_contact_page_married
   to_fee_page
   fee_page.submit_fee_no
   form_name_page.submit_valid_form_number
@@ -243,6 +243,22 @@ def to_contact_page
   claim_page.submit_claim_no
   dob_page.valid_partner_dob
   personal_details_page.submit_full_names
+  address_page.submit_full_address
+end
+
+def to_contact_page_single
+  to_fee_page
+  fee_page.submit_fee_no
+  form_name_page.submit_valid_form_number
+  applying_on_behalf_page.submit_no
+  national_insurance_page.select_yes_and_enter_valid_ni
+  marital_status_page.submit_single
+  savings_investment_page.low_amount_checked
+  benefit_page.submit_benefit_yes
+  probate_page.submit_probate_no
+  claim_page.submit_claim_no
+  dob_page.valid_dob
+  personal_details_page.submit_full_name
   address_page.submit_full_address
 end
 
