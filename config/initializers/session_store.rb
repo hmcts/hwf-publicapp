f43b75dcee4bb737fe0b6a0fc3026f94f4fe9a6b
DanielleKushnir <daniellekushnir22@gmail.com>
2025-01-16 14:45:02 +0000

    RST-6960 Added flags to Google Analytics cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,5 +3,9 @@
 Rails.application.config.session_store :cookie_store,
                                        key: '_hwf-publicapp_session',
                                        secure: Rails.env.production?,
-                                       domain: Rails.env.production? ? '.helpwithcourtfees.service.gov.uk' : nil,
+                                       domain: if Rails.env.production?
+                                                 '.helpwithcourtfees.service.gov.uk'
+                                               else
+                                                 '.hmcts.net'
+                                               end,
                                        same_site: :strict
