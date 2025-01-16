49d905265dab40d2cf2237fec1041c02f3e49def
DanielleKushnir <daniellekushnir22@gmail.com>
2025-01-16 15:19:08 +0000

    RST-6960 Removed ineffective flags and fixed Domain issues

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,9 +3,8 @@
 Rails.application.config.session_store :cookie_store,
                                        key: '_hwf-publicapp_session',
                                        secure: Rails.env.production?,
+                                       same_site: :strict,
                                        domain: if Rails.env.production?
-                                                 '.helpwithcourtfees.service.gov.uk'
-                                               else
-                                                 '.hmcts.net'
-                                               end,
-                                       same_site: :strict
+                                                 %w[.helpwithcourtfees.service.gov.uk
+                                                    .hmcts.net]
+                                               end
