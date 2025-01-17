46e374b517f080210a3e1936a5e10dd30fd0c4d5
sgmselli <sellings.matthew@gmail.com>
2025-01-17 13:10:52 +0000

    Rubocop fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,14 +33,14 @@ Rails.application.configure do
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new($stdout).
-    tap  { |logger| logger.formatter = Logger::Formatter.new }.
-    then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+                  tap  { |logger| logger.formatter = Logger::Formatter.new }.
+                  then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 
   # Log to STDOUT with the current request id as a default log tag.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
--- config/puma.rb
+++ config/puma.rb
@@ -45,4 +45,3 @@ port ENV.fetch("PORT", 3000)
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
-
