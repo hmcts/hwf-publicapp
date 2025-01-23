824cacf2963bcc8d3f267fb71cdc47328aea810b
DanielleKushnir <daniellekushnir22@gmail.com>
2025-01-23 15:46:28 +0000

    RST-6960 Correctly accept domains instead of array

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,9 +2,5 @@
 
 Rails.application.config.session_store :cookie_store,
                                        key: '_hwf-publicapp_session',
-                                       secure: Rails.env.production?,
                                        same_site: :strict,
-                                       domain: if Rails.env.production?
-                                                 %w[.helpwithcourtfees.service.gov.uk
-                                                    .hmcts.net]
-                                               end
+                                       domain: :all
