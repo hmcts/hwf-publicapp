880d2aa720b19dca4f727abc10fb4610bf46d208
DanielleKushnir <daniellekushnir22@gmail.com>
2025-01-27 11:04:46 +0000

    RST-6960 Check if cookie domain is valid

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,4 +3,16 @@
 Rails.application.config.session_store :cookie_store,
                                        key: '_hwf-publicapp_session',
                                        same_site: :strict,
-                                       domain: :all
+                                       secure: Rails.env.production?,
+                                       domain: ->(request) { determine_domain(request.host) }
+
+def determine_domain(host)
+  valid_domains =
+    %w[helpwithcourtfees.service.gov.uk hwf-publicapp.aat.platform.hmcts.net hwf-publicapp.demo.platform.hmcts.net]
+
+  valid_domains.each do |domain|
+    return domain if host.end_with?(domain)
+  end
+
+  nil
+end
