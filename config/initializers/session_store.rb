fb04a1d08b383d7b0475db15ab0ce970459a8a30
DanielleKushnir <124261069+DanielleKushnir@users.noreply.github.com>
2025-01-27 10:00:53 +0000

    RST-6960 Fix cookie rejection error (#1000)
    RST-6960 Fix cookie rejection error

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,5 @@
 
 Rails.application.config.session_store :cookie_store,
                                        key: '_hwf-publicapp_session',
-                                       same_site: :strict
+                                       same_site: :strict,
+                                       domain: :all
