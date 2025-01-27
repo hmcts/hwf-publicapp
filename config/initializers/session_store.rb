ae0bccbee6f3a4bfb04af999f11a5f8721ecca5a
Petr Zaparka <petr@zaparka.cz>
2025-01-27 13:19:08 +0000

    Revert "RST-6960 Fix cookie rejection error (#1000)" (#1010)
    This reverts commit fb04a1d08b383d7b0475db15ab0ce970459a8a30.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,5 +2,4 @@
 
 Rails.application.config.session_store :cookie_store,
                                        key: '_hwf-publicapp_session',
-                                       same_site: :strict,
-                                       domain: :all
+                                       same_site: :strict
