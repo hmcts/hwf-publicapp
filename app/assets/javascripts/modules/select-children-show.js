0ef0418d8068538c21d60318b8a9147ce5835dd8
Petr Zaparka <petr@zaparka.cz>
2025-02-03 11:33:23 +0000

    Updating JS to generate dropdowns

--- app/assets/javascripts/modules/select-children-show.js
+++ app/assets/javascripts/modules/select-children-show.js
@@ -3,7 +3,6 @@
 window.moj.Modules.SelectChildrenShow  = {
 
     selectors: {
-        selectBox: '#dependent_children_number',
         childrenNumberBox: '#dependent_children_age_band',
         children_age_range_box: 'children_age_range_box',
     },
@@ -11,24 +10,37 @@ window.moj.Modules.SelectChildrenShow  = {
     init: function() {
         const self = this;
 
-        self.loadRangeBoxes();
-        if(document.getElementById(self.selectors.selectBox).length) {
-            self.bindEvents();
-        }
+        self.bindEvents();
+
     },
 
     bindEvents: function() {
         const self = this;
-
-        document.getElementById(self.selectors.selectBox).on("change", (self.loadRangeBoxes).bind(self));
+        $('#dependent_children_number').on('change', function() {
+            const numberOfChildren = $(this).val();
+            console.log('Children number select changed to:', numberOfChildren);
+            self.updateRangeBoxes(numberOfChildren);
+        });
     },
 
-    loadRangeBoxes: function(e) {
-        const self = this;
+    updateRangeBoxes: function(numberOfChildren) {
+      const self = this;
+      const oneChildHtml = $('.children_age_select').first().html();
 
+      $('#dependent-children-age-panel .children_age_select').not(':first').remove(); // Clear existing appended elements
 
-        let tag_id = document.getElementById(self.selectors.children_age_range_box);
-        tag_id.innerHTML = "<%= escape_javascript(render('questions/forms/dependents/child_age_range_box')) %>";
-
+      // // numberOfChildrenSelects = numberOfChildren -1
+      for (let i = 0; i < numberOfChildren; i++) {
+        $('#dependent-children-age-panel .children_age_select').last().after(self.newDiv(oneChildHtml));
+      }
     },
+
+    newDiv: function(innerHTML) {
+      const self = this;
+      var newDiv = $('<div>', {
+        class: 'govuk-form-group children_age_select'
+      });
+      return newDiv.html(innerHTML);
+    },
+
 };
