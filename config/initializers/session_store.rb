f10e0296ceadf8e1380ceb2730e59c3481d8d74e
DanielleKushnir <124261069+DanielleKushnir@users.noreply.github.com>
2025-02-03 13:12:33 +0000

    RST-6960a Check if cookie domain is valid (#1009)
    RST-6960 Check if cookie domain is valid

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,16 @@
 
 Rails.application.config.session_store :cookie_store,
                                        key: '_hwf-publicapp_session',
-                                       same_site: :strict
+                                       same_site: :strict,
+                                       secure: Rails.env.production?,
+                                       domain: ->(request) { determine_domain(request.host) }
+
+def determine_domain(host)
+  valid_domains = Settings.session_store.valid_domains || []
+
+  valid_domains.each do |domain|
+    return domain if host.end_with?(domain)
+  end
+
+  nil
+end
