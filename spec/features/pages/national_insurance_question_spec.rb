3dd6c40b83f1eb3ed07839a3954caeb3c8641970
sgmselli <sellings.matthew@gmail.com>
2025-02-11 11:31:30 +0000

    Take user to yes radio instead of form when click error link

--- spec/features/pages/national_insurance_question_spec.rb
+++ spec/features/pages/national_insurance_question_spec.rb
@@ -31,7 +31,7 @@ RSpec.feature 'As a user' do
 
       describe 'clicking yes and leaving the ni field empty' do
         before do
-          choose 'national_insurance_has_ni_number_true'
+          choose 'national_insurance_has_ni_number'
           click_button 'Continue'
         end
 
@@ -42,7 +42,7 @@ RSpec.feature 'As a user' do
 
       describe 'providing an incorrect value' do
         before do
-          choose 'national_insurance_has_ni_number_true'
+          choose 'national_insurance_has_ni_number'
           fill_in 'national_insurance_number', with: 'AB123'
           click_button 'Continue'
         end
