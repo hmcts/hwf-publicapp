0057c503067a2c0b4ae74bd1358c5db7c46ed840
Petr Zaparka <petr@zaparka.cz>
2026-02-12 10:29:36 +0000

    Removing ZAP - might fix deployment (#1242)

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -4,10 +4,6 @@ Before('@hwf_submit_application') do
   stub_request(:post, "#{url}/api/submissions").to_return(status: 200, body: response.to_json)
 end
 
-Before('@zap') do
-  IO.popen('/Applications/ZAP\ 2.6.0.app/Contents/Java/zap.sh -daemon')
-end
-
 Before do
   stub_request(:any, 'https://dc.services.visualstudio.com/v2/track')
 end
